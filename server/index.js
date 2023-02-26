fb7cfdfa8c88a8a1f1ecf01ba638ee258742047b
NirbhaySirsikar <nirbhaysirsikar123@gmail.com>
2023-02-27 00:49:25 +0530

    updated api key and openai prompt also updated scripts in package.json for @NirbhaySirsikar

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-const  Api_key = 'b71850161eef473fb0e2c7a1b5a70722';
+const Api_key = 'b71850161eef473fb0e2c7a1b5a70722';
 
 const { text } = require('body-parser');
 const { response } = require('express');
@@ -17,9 +17,9 @@ const assemblyai = require('assemblyai');
 assemblyai.setAPIKey(Api_key)
 
 const { Configuration, OpenAIApi } = require('openai');
-const OPENAI_API_KEY = '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'
+const OPENAI_API_KEY = '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'
 const configuration = new Configuration({
-  apiKey:OPENAI_API_KEY,
+  apiKey: OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(configuration);
 
@@ -28,31 +28,31 @@ app.use(bodyParser.json());
 app.use(cors())
 app.use(express.json());
 
-app.get('/hello',(req, res, next) => {
+app.get('/hello', (req, res, next) => {
   res.send("Api working properly")
 })
 
-app.post('/api/messages', async(req, res) => {
+app.post('/api/messages', async (req, res) => {
   const message = req.body.message;
   console.log(`Received message: ${message}`);
-  
-  
+
+
   const info = await ytdl.getInfo(message);
   const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
   const audioUrl = await audioFormats[0].url;
   console.log(audioUrl)
-  
+
   const transcript = new assemblyai.Transcript();
   const respsone = await transcript.create({
-    audio_src_url:audioUrl,
-    
+    audio_src_url: audioUrl,
+
     options: {
       format_text: true || false
     }
-  
+
   })
 
-  const {id} = respsone.get()
+  const { id } = respsone.get()
   const data = await transcript.poll(id)
   var respsonejson = data.get();
   var datasend = respsonejson.text;
@@ -61,29 +61,29 @@ app.post('/api/messages', async(req, res) => {
   const summary = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `Please summarize the following text: "${datasend}`,
-    temperature:0,
+    temperature: 0,
     max_tokens: 60,
   })
-  
-  const senddata = summary.data.choices[0].text;
-  console.log(senddata)
-  
-  const quiz  = await openai.createCompletion({
+
+  const summarizeddata = summary.data.choices[0].text;
+  console.log(summarizeddata)
+
+  const quiz = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Please make quiz from this text: "${datasend}`,
-    temperature:0,
+    prompt: `Please make quiz of atleast three questions from this text, give me four options for each and don't tell me the answers: "${datasend}`,
+    temperature: 0,
     max_tokens: 600,
 
   })
 
   const quizdata = quiz.data.choices[0].text;
   console.log(quizdata)
-  res.json(senddata)
+  res.json(summarizeddata)
 
 });
 
 
-app.listen(port , () => {
-    console.log(`Express.js app listening at ${port}`)
+app.listen(port, () => {
+  console.log(`Express.js app listening at ${port}`)
 })
 
