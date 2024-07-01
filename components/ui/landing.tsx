05cdf8cc5b04d988febb6b42abb1a80365141606
NirbhaySirsikar <nirbhaysirsikar123@gmail.com>
2024-07-01 22:46:26 +0530

    fixed landing page ui bug

--- components/ui/landing.tsx
+++ components/ui/landing.tsx
@@ -87,7 +87,7 @@ export function Landing() {
   // }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#1a1b1e] text-white">
-      <div className="h-full w-full dark:bg-[#1a1b1e] bg-white  dark:bg-grid-small-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-[100vh] w-full dark:bg-[#1a1b1e] bg-white  dark:bg-grid-small-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-[#1a1b1e] bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div className="max-w-2xl mx-auto p-4">
           <h1 className="relative z-10 text-6xl md:text-7xl text-center font-sans font-bold">
