f511d89558e901b19dfef4227bbdb1c5f247b229
NirbhaySirsikar <nirbhaysirsikar123@gmail.com>
2024-07-04 13:16:06 +0530

    Fixed backend call issue

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90 whitespace-normal h-auto",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90 whitespace-normal h-auto",
         outline:
-          "border border-input bg-zinc-900 hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-zinc-900 hover:bg-accent hover:text-accent-foreground whitespace-normal h-auto",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
