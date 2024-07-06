590adecac69e4137cda03f94d43f1fbaedbe513d
NirbhaySirsikar <nirbhaysirsikar123@gmail.com>
2024-07-06 18:34:08 +0530

    updated favicon and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-"use client"
 import { Landing } from '@/components/ui/landing'
-import { BackgroundGradient } from '@/components/ui/background-gradient'
-import { useState } from 'react'
-// import ytdl from 'ytdl-core'
 
 function Home() {
   return (
